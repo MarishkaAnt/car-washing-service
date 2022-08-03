323772e31ce676e8bf3ff1e33cc55e5502f3bd03
Antonova Mariia <mantonova@fil-it.ru>
2022-08-03 22:26:38 +0300

    US00-T04-ST01 Создать Entity User

---
+++ src/main/java/org/philosophy/carwashing/model/User.java
@@ -0,0 +1,29 @@
+package org.philosophy.carwashing.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private Boolean hasPersonDiscount;
+    private Boolean isActive;
+    private BigDecimal discountAmount;
+}

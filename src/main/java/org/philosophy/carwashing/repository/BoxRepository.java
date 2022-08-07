773b7b44253f4c16dfa91fd3c0209b173cdde02c
Antonova Mariia <mantonova@fil-it.ru>
2022-08-07 11:58:51 +0300

    US01-T01-ST02 Создать BoxRepository (попытка решить проболему n+1)

--- src/main/java/org/philosophy/carwashing/repository/BoxRepository.java
+++ src/main/java/org/philosophy/carwashing/repository/BoxRepository.java
@@ -2,8 +2,16 @@ package org.philosophy.carwashing.repository;
 
 import org.philosophy.carwashing.model.Box;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoxRepository extends JpaRepository<Box, Integer> {
+    @Override
+    @NonNull
+    @Query("select b from Box as b join BoxType bt on b.boxType.id=bt.id")
+    List<Box> findAll();
 }

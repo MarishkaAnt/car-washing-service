8121e3a9211cd728b37fb56a699ce19c508431f3
Antonova Mariia <mantonova@fil-it.ru>
2022-08-07 18:16:09 +0300

    US02-T01-ST02 Создать BookingRepository

---
+++ src/main/java/org/philosophy/carwashing/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package org.philosophy.carwashing.repository;
+
+import org.philosophy.carwashing.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Integer> {
+}

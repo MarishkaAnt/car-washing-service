474db8fc6e46d2fbd269896d85a077767da02882
Antonova Mariia <mantonova@fil-it.ru>
2022-08-08 15:28:48 +0300

    US00-T07 Role - удалена сущность и все её использования

--- src/main/java/org/philosophy/carwashing/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package org.philosophy.carwashing.repository;
-
-import org.philosophy.carwashing.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}

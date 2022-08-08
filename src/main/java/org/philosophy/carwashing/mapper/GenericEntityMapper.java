df5541fb0da5d4b4e8a95a2225c32b747dffd2bf
Antonova Mariia <mantonova@fil-it.ru>
2022-08-08 12:47:01 +0300

    US00-T04 удален ToDo комментарий

--- src/main/java/org/philosophy/carwashing/mapper/GenericEntityMapper.java
+++ src/main/java/org/philosophy/carwashing/mapper/GenericEntityMapper.java
@@ -1,14 +1,5 @@
 package org.philosophy.carwashing.mapper;
 
-/*
-ToDo добавить в мапперы, в которых поля не совпадают
-
-    @Mapping(target="employeeId", source="entity.id")
-    EmployeeDTO toDTO(Employee entity);
-
-    @Mapping(target="id", source="dto.employeeId")
-    Employee toEntity(EmployeeDTO dto);
- */
 public interface GenericEntityMapper <E, D>{
 
     E toEntity(D d);

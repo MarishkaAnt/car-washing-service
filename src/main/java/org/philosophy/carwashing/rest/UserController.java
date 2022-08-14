9f53ebf5d4c82b4aea497af78ec84bfd888afb97
Antonova Mariia <mantonova@fil-it.ru>
2022-08-14 22:33:14 +0300

    добавлен апдейт метод для всех полей и для роли

--- src/main/java/org/philosophy/carwashing/rest/UserController.java
+++ src/main/java/org/philosophy/carwashing/rest/UserController.java
@@ -1,9 +1,9 @@
 package org.philosophy.carwashing.rest;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.philosophy.carwashing.dto.requestdto.UserSignInRequestDto;
 import org.philosophy.carwashing.dto.responsedto.UserViewResponseDto;
+import org.philosophy.carwashing.enums.Roles;
 import org.philosophy.carwashing.service.UserServiceImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,32 +15,47 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/users")
 public class UserController {
 
-    private final UserServiceImpl bookingServiceImpl;
+    private final UserServiceImpl userService;
 
     @GetMapping
     public ResponseEntity<Page<UserViewResponseDto>> getAll(Pageable pageable){
-        Page<UserViewResponseDto> dtos = bookingServiceImpl.findAll(pageable);
+        Page<UserViewResponseDto> dtos = userService.findAll(pageable);
         return ResponseEntity.ok().body(dtos);
     }
 
 
     @GetMapping("/{id}")
     public ResponseEntity<UserViewResponseDto> getById(@PathVariable Integer id){
-        UserViewResponseDto responseDto = bookingServiceImpl.findById(id);
+        UserViewResponseDto responseDto = userService.findById(id);
         return ResponseEntity.ok().body(responseDto);
     }
 
     @PostMapping
-    //@Secured(ROLE_ADMIN)
     public ResponseEntity<UserViewResponseDto> create(@RequestBody UserSignInRequestDto dto) {
-        UserViewResponseDto responseDto = bookingServiceImpl.create(dto);
+        UserViewResponseDto responseDto = userService.create(dto);
         return ResponseEntity.ok().body(responseDto);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteById(@PathVariable Integer id){
-        bookingServiceImpl.deleteById(id);
+    public ResponseEntity<Void> deleteById(@PathVariable Integer id){
+        userService.deleteById(id);
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserViewResponseDto> update(@RequestBody UserSignInRequestDto dto,
+                                                      @PathVariable Integer id) {
+        UserViewResponseDto responseDto = userService.update(id, dto);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserViewResponseDto> changeRole(@RequestBody String role,
+                                                      @PathVariable Integer id) {
+        UserViewResponseDto responseDto = userService.changeRole(id, Roles.valueOf(role));
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+
 
 }
--- src/main/java/org/philosophy/carwashing/service/UserServiceImpl.java
+++ src/main/java/org/philosophy/carwashing/service/UserServiceImpl.java
@@ -3,19 +3,20 @@ package org.philosophy.carwashing.service;
 import lombok.RequiredArgsConstructor;
 import org.philosophy.carwashing.dto.requestdto.UserSignInRequestDto;
 import org.philosophy.carwashing.dto.responsedto.UserViewResponseDto;
+import org.philosophy.carwashing.enums.Roles;
 import org.philosophy.carwashing.mapper.request.UserSignInRequestDtoMapper;
 import org.philosophy.carwashing.mapper.response.UserViewResponseMapper;
-import org.philosophy.carwashing.model.Booking;
 import org.philosophy.carwashing.model.User;
 import org.philosophy.carwashing.repository.UserRepository;
 import org.philosophy.carwashing.validator.ParameterValidator;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,13 +27,19 @@ public class UserServiceImpl implements GenericService<Integer,
     private final UserSignInRequestDtoMapper requestDtoMapper;
     private final UserViewResponseMapper responseMapper;
     private final ParameterValidator<UserSignInRequestDto> validator;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserViewResponseDto create(UserSignInRequestDto dto) {
         validator.validateDtoNotNull(dto);
-
-
-        return null;
+        User user = requestDtoMapper.toEntity(dto);
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setIsActive(true);
+        user.setRole(Roles.USER);
+        user.setHasDiscount(false);
+        user.setDiscountAmount(0);
+        User saved = userRepository.save(user);
+        return responseMapper.toDto(saved);
     }
 
     @Override
@@ -40,12 +47,7 @@ public class UserServiceImpl implements GenericService<Integer,
     public void deleteById(Integer id) {
         validator.validateIdIsNullOrNegative(id);
         User user = userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-//        List<Booking> bookings = user.getBookings();
-//        if(bookings.size() != 0) {
             user.setIsActive(false);
-//        } else {
-//            userRepository.deleteById(id);
-//        }
     }
 
     @Override
@@ -61,12 +63,33 @@ public class UserServiceImpl implements GenericService<Integer,
         return userRepository.findAll(pageable)
                 .map(responseMapper::toDto);
     }
-/*
-ToDo
- */
 
     @Override
-    public UserViewResponseDto update(Integer integer, UserSignInRequestDto dto) {
-        return null;
+    public UserViewResponseDto update(Integer id, UserSignInRequestDto dto) {
+        User user = userRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        if (dto.getFirstName() != null){
+            user.setFirstName(dto.getFirstName());
+        }
+        if (dto.getLastName() != null){
+            user.setLastName(dto.getLastName());
+        }
+        if (dto.getEmail() != null){
+            user.setEmail(dto.getEmail());
+        }
+        if (dto.getPassword() != null){
+            user.setEmail(passwordEncoder.encode(dto.getPassword()));
+        }
+        User saved = userRepository.save(user);
+        return responseMapper.toDto(saved);
+    }
+
+    @Secured("ROLE_ADMIN")
+    public UserViewResponseDto changeRole(Integer id, Roles role){
+        User user = userRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        user.setRole(role);
+        User saved = userRepository.save(user);
+        return responseMapper.toDto(saved);
     }
 }

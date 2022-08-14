a9ea9e5bcfa0c80927ec23730981019d4f2d990d
Antonova Mariia <mantonova@fil-it.ru>
2022-08-14 18:59:17 +0300

    удалены лишние методы

--- src/main/java/org/philosophy/carwashing/repository/BoxRepository.java
+++ src/main/java/org/philosophy/carwashing/repository/BoxRepository.java
@@ -5,22 +5,17 @@ import org.philosophy.carwashing.model.Box;
 import org.philosophy.carwashing.model.BoxType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface BoxRepository extends JpaRepository<Box, Integer>, JpaSpecificationExecutor<Box> {
-    @Override
-    @NonNull
-    @Query("select b, bt from Box as b join BoxType bt on b.boxType.id=bt.id")
-    List<Box> findAll();
-
-    @NonNull
-    @Query("select b, bt from Box as b join BoxType bt on b.boxType.id=bt.id where b.boxType=?1")
-    List<Box> findAllByBoxType(BoxType boxType);
 
     @NonNull
     @Query("select b from Box as b order by b.id asc")

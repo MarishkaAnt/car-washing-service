4d2b3e8c49f29801ff6570046e553fb4e48e491a
Antonova Mariia <mantonova@fil-it.ru>
2022-08-14 18:55:35 +0300

    добавлены константы макс и мин значений для поля скидки и дополнительные константы сообщений об ошибках

--- src/test/java/org/philosophy/carwashing/service/utilits/StringCommonConstants.java
+++ src/test/java/org/philosophy/carwashing/service/utilits/StringCommonConstants.java
@@ -1,5 +1,6 @@
 package org.philosophy.carwashing.service.utilits;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -8,16 +9,11 @@ public class StringCommonConstants {
     public static final int CORRECT_ID_EQUALS_1 = 1;
     public static final double SPEEDCOEFFICIENT_EQUAL_1 = 1.0;
     public static final LocalTime CORRECT_OPEN_TIME_8_00 = LocalTime.parse("08:00");
-    public static final LocalDateTime CORRECT_REQUEST_START_DATE_TIME = LocalDateTime.parse("2022-08-09T09:00");
     public static final LocalTime CORRECT_CLOSE_TIME_21_00 = LocalTime.parse("21:00");
-    public static final LocalDateTime CORRECT_REQUEST_END_DATE_TIME = LocalDateTime.parse("2022-08-09T21:00");
-    public static final int SAVED_ENTITY_ID_EQUAL_3 = 3;
     public static final Duration CORRECT_DURATION_PT_15_M = Duration.parse("PT15M");
-    public static final double CORRECT_COST_300 = 300.0;
+    public static final BigDecimal CORRECT_COST_300 = BigDecimal.valueOf(300.0);
     public static final LocalDateTime DATETIME_FROM_10 = LocalDateTime.parse("2022-08-09T10:00:00");
     public static final LocalDateTime DATE_TIME_TO_10_15 = LocalDateTime.parse("2022-08-09T10:15:00");
     public static final Duration PT_15_M = Duration.parse("PT15M");
-    public static final LocalDateTime RESPONSE_DATETIME_FROM = LocalDateTime.parse("2022-08-09T09:00:00");
-    public static final LocalDateTime RESPONSE_DATETIME_TO = LocalDateTime.parse("2022-08-09T09:15:00");
 
 }

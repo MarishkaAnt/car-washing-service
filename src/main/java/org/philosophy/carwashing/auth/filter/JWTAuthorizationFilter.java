02b51a6a7e3b35f13dc84a8dc33f7a6f27dbe3e9
Antonova Mariia <mantonova@fil-it.ru>
2022-08-16 10:09:24 +0300

    изменен путь с api/v1/login на /login

--- src/main/java/org/philosophy/carwashing/auth/filter/JWTAuthorizationFilter.java
+++ src/main/java/org/philosophy/carwashing/auth/filter/JWTAuthorizationFilter.java
@@ -3,10 +3,8 @@ package org.philosophy.carwashing.auth.filter;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,7 +29,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/v1/login")) {
+        if (request.getServletPath().equals("/login")) {
             filterChain.doFilter(request, response);
         } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
